8ec1700f59b48977e7dc1b7c9b53c117527c5327
nhochuy <buigiahuy22092019@gmail.com>
2024-02-16 15:55:56 +0700

    update

--- pythonProject/app.py
+++ pythonProject/app.py
@@ -6,5 +6,21 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/pha-do')
+def genealogical():
+    return render_template('genealogical.html')
+
+@app.route('/lich-su')
+def hitory():
+    return render_template('hitory.html')
+
+@app.route('/su-kien')
+def event():
+    return render_template('event.html')
+
+@app.route('/chi-tiet')
+def event_detail():
+    return render_template('event-detail.html')
+
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
